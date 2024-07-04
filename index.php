cc828f23c518d1433f2ecbbd64bc74fd6212595f
Mateus Merçon <mateusmercon@gmail.com>
2024-07-04 11:49:09 -0300

    feat: inserir headers de segurança

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+header("X-XSS-Protection: 1; mode=block");
+header("X-Frame-Options: DENY");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: no-referrer");
+header("Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:;");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
